643871639fc22a017159c5f09f75a29173be24bb
Ivan <ivanchernetskey@mail.ru>
2021-03-16 20:17:50 +0300

    connect model and view (counter)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './styles.css'
+
+const counter = document.getElementById('counter')
+
+let state = 0
+
+function render() {
+    counter.textContent = state.toString() //пишу toString для того чтобы вебшторм не ругался и явно привожу к строке
+}
+
+render()
