1a9e62792635d5309dab1b4f47102a76bc52769a
Ivan <ivanchernetskey@mail.ru>
2021-03-16 20:21:30 +0300

    add options to the buttons(reminds me React)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import './styles.css'
 
 const counter = document.getElementById('counter')
+const addBtn = document.getElementById('add')
+const subBtn = document.getElementById('sub')
 
 let state = 0
 
 function render() {
     counter.textContent = state.toString() //пишу toString для того чтобы вебшторм не ругался и явно привожу к строке
 }
+
+addBtn.addEventListener('click', () => {
+    state++
+    render()
+})
+
+subBtn.addEventListener('click', () => {
+    state--
+    render()
+})
 
 render()
